18fdfb82cf44e5b4825989a8d433f88ba58ec460
BllsszzXD <filecloudrom@gmail.com>
2025-12-24 00:16:30 +0700

    X00TD : Regenerate

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,15 +1,1 @@
-#!/bin/bash
 
-# Clone kernel tree
-#rm -rf kernel/asus/sdm660
-git clone --depth=1 --recursive https://github.com/Tiktodz/android_kernel_asus_sdm660 kernel/asus/sdm660
-
-# Clone common tree
-#rm -rf device/asus/sdm660-common
-git clone --depth=1https://github.com/afl-Workground/android_device_asus_sdm660-common -b 16 device/asus/sdm660-common
-
-# Clone vendor tree
-#rm -rf vendor/asus
-git clone --depth=1 https://github.com/afl-Workground/proprietary_vendor_asus -b 16 vendor/asus
-
-export TZ=Asia/Jakarta
