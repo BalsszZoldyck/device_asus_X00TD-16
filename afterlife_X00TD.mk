26a5b9bb4488066336889c6c6c3430238581442a
BllsszzXD <filecloudrom@gmail.com>
2025-12-24 00:18:41 +0700

    X00TD : Regenerate

--- afterlife_X00TD.mk
+++ afterlife_X00TD.mk
@@ -10,24 +10,19 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk)
 
 # Inherit some common crDroid stuff
-$(call inherit-product, vendor/afterlife/config/common_full_phone.mk)
-
-# EvoX flags.
-TARGET_BOOT_ANIMATION_RES := 1080
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Inherit from X00TD device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
 
-# GApps flags.
-WITH_GMS := true
-
 # Inherit some common device props
+TARGET_BOOT_ANIMATION_RES := 1080
 FULL_SYSTEM_OPTIMIZE_JAVA := true
 SYSTEM_OPTIMIZE_JAVA := true
 SYSTEMUI_OPTIMIZE_JAVA := true
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := afterlife_X00TD
+PRODUCT_NAME := lineage_X00TD
 PRODUCT_DEVICE := X00TD
 PRODUCT_BRAND := asus
 PRODUCT_MODEL := Zenfone Max Pro M1
@@ -36,4 +31,7 @@ PRODUCT_MANUFACTURER := asus
 PRODUCT_GMS_CLIENTID_BASE := android-asus
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    DeviceProduct=ASUS_X00TD
+    BuildDesc="sdm660_64-user 10 QKQ1 72 release-keys" \
+    DeviceProduct=$(PRODUCT_SYSTEM_NAME) \
+    DeviceName=$(PRODUCT_SYSTEM_DEVICE) \
+    BuildFingerprint=asus/ASUS_X00TD/ASUS_X00TD:10/QKQ1/17.2017.2012.438-20201203:user/release-keys
